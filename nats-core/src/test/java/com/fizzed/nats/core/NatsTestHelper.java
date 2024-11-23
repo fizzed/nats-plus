8072d44baceac72bcb1333662b21e7f34b396d5f
Joe Lauer <joe@lauer.bz>
2024-11-22 22:32:27 -0500

    Fix unit tests to use new resources

--- nats-core/src/test/java/com/fizzed/nats/core/NatsTestHelper.java
+++ nats-core/src/test/java/com/fizzed/nats/core/NatsTestHelper.java
@@ -1,10 +1,10 @@
 package com.fizzed.nats.core;
 
-import com.fizzed.jne.JNE;
+import com.fizzed.crux.util.Resources;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 class NatsTestHelper {
@@ -21,12 +21,17 @@ class NatsTestHelper {
     static public Path getOrFindNatsExe() {
         if (natsExe == null) {
             try {
-                File f = JNE.findExecutable("nats-server");
-                if (f != null) {
-                    natsExe = f.toPath();
-                } else {
-                    throw new RuntimeException("Could not find nats exe");
+                Path markerFile = Resources.file("/locator.txt");
+                Path resourcesDir = markerFile.resolve("../../../../.resources").normalize().toAbsolutePath();
+                Path maybeExe = resourcesDir.resolve("nats-server");
+                if (!Files.exists(maybeExe)) {
+                    maybeExe = resourcesDir.resolve("nats-server.exe");
                 }
+                if (!Files.exists(maybeExe)) {
+                    throw new RuntimeException("Unable to locate .resources/nats-server (did you run 'blaze setup' ??)");
+                }
+
+                natsExe = maybeExe;
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
