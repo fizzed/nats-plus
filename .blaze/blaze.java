d59fb873a21bb9a5d570a169c2537a738d55fa2d
Joe Lauer <joe@lauer.bz>
2025-01-15 11:49:39 -0500

    Add buildx support for cross platform testing

--- .blaze/blaze.java
+++ .blaze/blaze.java
@@ -1,15 +1,20 @@
 import com.fizzed.blaze.Config;
 import com.fizzed.blaze.Contexts;
+import com.fizzed.blaze.Task;
+import com.fizzed.buildx.Buildx;
+import com.fizzed.buildx.Target;
 import com.fizzed.jne.HardwareArchitecture;
 import com.fizzed.jne.NativeLanguageModel;
 import com.fizzed.jne.NativeTarget;
 import com.fizzed.jne.OperatingSystem;
 import org.slf4j.Logger;
 import java.nio.file.Path;
+import java.util.List;
 
 import static com.fizzed.blaze.Archives.unarchive;
 import static com.fizzed.blaze.Https.*;
 import static com.fizzed.blaze.Systems.*;
+import static java.util.Arrays.asList;
 
 public class blaze {
     private final Logger log = Contexts.logger();
@@ -32,6 +37,7 @@ public class blaze {
         final NativeLanguageModel nlm = new NativeLanguageModel()
             .add("version", this.natsVersion)
             .add(OperatingSystem.MACOS, "darwin")
+            .add(HardwareArchitecture.ARMHF, "arm7")
             .add(HardwareArchitecture.X64, "amd64")
             .add(HardwareArchitecture.X32, "386");
 
@@ -73,6 +79,32 @@ public class blaze {
             rm(tempDownloadDir).recursive().force().run();
             log.info("Deleted temp download directory: " + tempDownloadDir);
         }
+    }
+
+    private final List<Target> crossTestTargets = asList(
+        new Target("linux", "x64").setTags("test").setHost("bmh-build-x64-linux-latest"),
+        new Target("linux", "arm64").setTags("test").setHost("bmh-build-arm64-linux-latest"),
+        //new Target("linux", "riscv64").setTags("test").setHost("bmh-build-riscv64-linux-latest"),
+        //new Target("linux", "armhf").setTags("test").setHost("bmh-build-armhf-linux-latest"),
+        new Target("linux_musl", "x64").setTags("test").setHost("bmh-build-x64-linux-musl-latest"),
+        new Target("macos", "x64").setTags("test").setHost("bmh-build-x64-macos-latest"),
+        new Target("macos", "arm64").setTags("test").setHost("bmh-build-arm64-macos-latest"),
+        new Target("windows", "x64").setTags("test").setHost("bmh-build-x64-windows-latest"),
+        new Target("windows", "arm64").setTags("test").setHost("bmh-build-arm64-windows-latest"),
+        new Target("freebsd", "x64").setTags("test").setHost("bmh-build-x64-freebsd-latest")
+        //new Target("openbsd", "x64").setTags("test").setHost("bmh-build-x64-openbsd-latest")
+    );
+
+    @Task(order = 53)
+    public void cross_tests() throws Exception {
+        new Buildx(crossTestTargets)
+            .tags("test")
+            .execute((target, project) -> {
+                project.action("java", "-jar", "blaze.jar", "setup")
+                    .run();
+                project.action("mvn", "clean", "test")
+                    .run();
+            });
     }
 
 }
